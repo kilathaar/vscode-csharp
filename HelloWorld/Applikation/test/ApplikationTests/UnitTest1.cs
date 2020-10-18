68cdc9a23ca7f946528952ed0f38ba97fdbec765
Jimmy Ljungberg <jimmy.ljungberg@gmail.com>
2020-10-18 16:34:07 +0200

    Kodformattering

--- HelloWorld/Applikation/test/ApplikationTests/UnitTest1.cs
+++ HelloWorld/Applikation/test/ApplikationTests/UnitTest1.cs
@@ -1,18 +1,14 @@
 using NUnit.Framework;
 
-namespace ApplikationTests
-{
-    public class Tests
-    {
-        [SetUp]
-        public void Setup()
-        {
-        }
+namespace ApplikationTests {
+	public class Tests {
+		[SetUp]
+		public void Setup() {
+		}
 
-        [Test]
-        public void Test1()
-        {
-            Assert.Pass();
-        }
-    }
+		[Test]
+		public void Test1() {
+			Assert.Pass();
+		}
+	}
 }
